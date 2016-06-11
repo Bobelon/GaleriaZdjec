4fbde2a4c9bd991b82edc1e1130507d77734a967
FilipKasprzycki <kasprzycki.filip@gmail.com>
2016-06-11 21:15:30 +0200

    Rozpoznawanie zmiany położenia obiektu

--- Galeria.cpp
+++ Galeria.cpp
@@ -5,16 +5,28 @@
 using namespace cv;
 using namespace std;
 
-/* Funkcka files zapożyczona z 
+/* Funkcja files zapożyczona z 
  * http://cpp0x.pl/artykuly/?id=53
  * */
 void files (const char * dirPath ) {
     struct dirent * file;
     DIR * path;
-   
+    char str [100] = "Test";
+    
+    char *rozszezenie = (char *) malloc (1024 * sizeof (char));
+	char *pom = (char *) malloc (1024 * sizeof (char));
+    
     if (path = opendir (dirPath)) {
-        while (file = readdir (path))
-             puts (file -> d_name);
+        while (file = readdir (path)) {
+			pom = strchr (file->d_name,'.');
+			cout << file->d_name << endl;
+			//strcpy (rozszezenie, pom);
+			/*
+			if (strcpy (rozszezenie, "jpg") || strcpy (rozszezenie, "png")) {
+				cout << file->d_name << endl;
+			}*/
+		}
+             //puts (file -> d_name);
        
         closedir( path );
     }
@@ -27,8 +39,12 @@ int main( int argc, char** argv ) {
 	double width;
 	double height;
 	double fps;
+	int pozPocz = -1;
+	clock_t aktualnyCzas = clock() / CLOCKS_PER_SEC;
+			//( clock () - begin_time ) /  CLOCKS_PER_SEC;
+
 	
-	//files (".");
+	files (".");
 	
 	namedWindow ("Wspaniała galeria zdjęć", CV_WINDOW_AUTOSIZE); // Tworzenie nowego okna
 	//namedWindow ("Testowe",  CV_WINDOW_AUTOSIZE);
@@ -51,18 +67,19 @@ int main( int argc, char** argv ) {
 		
 		
 		
-	// Główna pętla programu
-	while(true) {
 		int key;
 		Mat bgr, hsv, mirror, colorDetection, countour, frame;
 		Mat frame2;
+	// Główna pętla programu
+	while(true) {
+		
 		
 		if (cap.read(bgr)) {
 			//blur( bgr, bgr, Size(10,10) );
 			cvtColor( bgr, hsv, CV_BGR2HSV); // filtr zmieniający skalę barw
 			flip(hsv, mirror, 1); // Odbicie lustrzane
 			//
-			//blur( frame, frame, Size(10,10) );
+			//blur( mirror, mirror, Size(10,10) );
 			//erode(frame, frame, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
 			//dilate(frame, frame, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
 			inRange (mirror, Scalar (60, 200, 130), Scalar (100, 255, 255), colorDetection);
@@ -79,11 +96,59 @@ int main( int argc, char** argv ) {
 				trzy = 20;
 				cztery = 3;
 			Canny( colorDetection, colorDetection, jeden, dwa * trzy, cztery ); // Tworzenie konturu
-			vector<vector<Point> > contours;
+			vector <vector <Point> > contours;
 			findContours( colorDetection, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);	
 			drawContours( colorDetection, contours, 0, Scalar(255,0,0),5,8);
+			
+			
+			/// Momenty :)
+		vector<Moments> mu(contours.size() );
+		for( int i = 0; i < contours.size(); i++ ) {
+			mu[i] = moments( contours[i], false ); 
+		}
+        
+		
+		if (mu.size() > 0) {
+			Point2f mc; // mass center
+			int maxI = 0;
+			
+			int x, y, przesuniecie;
+						x = mu[maxI].m10/mu[maxI].m00;
+						y = mu[maxI].m01/mu[maxI].m00;
+						przesuniecie = 200;
+					//cout << ((int)  aktualnyCzas / 1000000)<< "   " << clock() <<  endl;
+					if (pozPocz == -1) {
+						pozPocz = x;
+					}					
+					if ((x < 250) && (((double) (clock () - aktualnyCzas) / CLOCKS_PER_SEC) >= 0.5)) {
+						cout << "LEWO" << endl;
+						pozPocz = x;
+						aktualnyCzas = clock();
+					}
+					if ((x > 250) && (((double) (clock () - aktualnyCzas) / CLOCKS_PER_SEC) >= 0.5)) {
+						cout << "PRAWO" << endl;
+						pozPocz = x;
+						aktualnyCzas = clock();
+					}
+			
+			
+			mc = Point2f( x , y );
+			for( int i = 0; i < contours.size(); i++ ) {
+				if (contourArea(contours[maxI]) < contourArea(contours[i])) {
+					maxI = i;
+					mc = Point2f( mu[maxI].m10/mu[maxI].m00, mu[maxI].m01/mu[maxI].m00);
+					
+					
+					
+				} 
+			}
+			//circle( colorDetection, mc, sqrt(contourArea(contours[maxI])), Scalar(255,0,0) );
+		}
+		
+		
+			
 			imshow ("Wspaniała galeria zdjęć", colorDetection);
-			//imshow ("Testowe", mirror);
+			imshow ("Testowe", mirror);
 		}
 		else
 			break;
