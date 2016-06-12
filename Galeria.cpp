a2f6513cdf045badd55425289035020357de58f9
FilipKasprzycki <kasprzycki.filip@gmail.com>
2016-06-12 12:29:06 +0200

    Poprawki kosmetyczne - projekt gotowy

--- Galeria.cpp
+++ Galeria.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <dirent.h>
-#include <opencv2/opencv.hpp>
-
-#include <opencv2/highgui/highgui.hpp>
 #include <list>
-#include <vector>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace cv;
 using namespace std;
@@ -12,40 +11,49 @@ using namespace std;
 list <string> listOfImages;
 list<string>::iterator iter;
 
-
+// Tworzenie listy zdjęć
 void files (const char * dirPath ) {
 	
-    struct dirent * file;
-    DIR * path;
-    
-    
-    char *rozszezenie = (char *) malloc (1024 * sizeof (char));
-	char *pom = (char *) malloc (1024 * sizeof (char));
-	
+    struct dirent *file;
+    DIR *path;
     
     if (path = opendir (dirPath)) {
-        while (file = readdir (path)) {
-			pom = strchr (file->d_name,'.');
-			listOfImages.push_back (file->d_name);
-			//strcpy (rozszezenie, pom);
-			/*
-			if (strcpy (rozszezenie, "jpg") || strcpy (rozszezenie, "png")) {
+		
+		char *type = (char *) malloc (4 * sizeof (char));
+		
+		cout << "GALERIA ZDJEC:\n\n";
+		
+		// Dodawanie do listy tylko plików z rozszezeniem *jpg
+		while (file = readdir (path)) {
+			
+			if (strcmp (file->d_name, ".") != 0 && strcmp (file->d_name, "..") != 0) {
+				listOfImages.push_back (file->d_name);
 				cout << file->d_name << endl;
-			}*/
+			}
+			
+			// NIE DZIAŁA
+			/*
+			strcpy (type, strchr (file->d_name, '.')); 		
+			
+			if (strcmp (type, ".jpg") == 0) {
+				listOfImages.push_back (file->d_name);
+				cout << file->d_name << endl;
+			}
+			* */
 		}
-       
-        closedir( path );
-    }
-    
+        closedir (path);
+        
+        listOfImages.sort();
+    }    
     else {
 		cout << "Nieprawidlowa sciezka" << endl;
 	}
 }
 
+// Wyświetlanie obrazka
 void showPicture (bool next) {
+	destroyWindow (*iter);
 	
-	
-	//cout << "\n\n\nXXDXDXDDD\n\n\n\n";
 	if (next) {
 		iter++;
 		if (iter == listOfImages.end()) {
@@ -59,21 +67,12 @@ void showPicture (bool next) {
 		iter--;
 	}
 	
+	Mat image = imread ("./CL/" + *iter, CV_LOAD_IMAGE_COLOR);
 	
-	
-	Mat imageCL;
-	string name = "./CL/" + *iter;
-	
-	imageCL = imread (name, CV_LOAD_IMAGE_COLOR);
-	
-	if (!imageCL.data) {
-		cout << "BŁĄD" << endl;
-		cout << name << endl;
+	if (image.data) {
+		imshow (*iter, image);
+		moveWindow (*iter, 0, 0);
 	}
-	else {
-	cout << "showPicture" << endl;
-	imshow ("CL", imageCL);
-}
 }
 
 int main( int argc, char** argv ) {
@@ -81,32 +80,17 @@ int main( int argc, char** argv ) {
 	double width;
 	double height;
 	double fps;
-	int pozPocz = -1;
-	clock_t aktualnyCzas = clock() / CLOCKS_PER_SEC;
-			//( clock () - begin_time ) /  CLOCKS_PER_SEC;
+	clock_t currentTime = clock() / CLOCKS_PER_SEC;
 
-	
-	files ("./CL");
-	
-	namedWindow ("CL", CV_WINDOW_AUTOSIZE);
-	
+	if (argc > 1)
+		files (argv[1]);
+	else
+		files ("./CL");	
 	
 	iter = listOfImages.begin();
 	
-	//cout << *iter << endl;
-	//cout << *++iter << endl;
-	//wskaznik++;
-	//for (it = listOfImages.begin(); it != listOfImages.end(); ++it) {
-	//	cout << *it << endl;
-	//}
-	//cout << "Ilość znalezionych plików: " << listOfImages.front() << endl;
-	//cout << "Ilość znalezionych plików: " << (listOfImages.front() + 1) << endl;
 	
-	//string imageName = *iter;
 	
-	//namedWindow (*iter, CV_WINDOW_AUTOSIZE);
-	namedWindow ("Wspaniała galeria zdjęć", CV_WINDOW_AUTOSIZE); // Tworzenie nowego okna
-	namedWindow ("Testowe",  CV_WINDOW_AUTOSIZE);
 	VideoCapture cap(0);
 	
 	// Błąd dostępu do kamerki
@@ -118,36 +102,26 @@ int main( int argc, char** argv ) {
 	height = cap.get (CV_CAP_PROP_FRAME_HEIGHT);
 	fps = cap.get (CV_CAP_PROP_FPS);
 	
-	// Mało ważna informacja w terminalu
-	cout << "Parametry wideo : " << width<< " x " << height << " @ " << fps << " fps"<< endl;
-	
 	if (fps < 0)
 		fps = 30; // Tak na wszelki wypadek :)
 		
 		
 		
-		int key;
-		Mat bgr, hsv, mirror, colorDetection, countour, frame;
-		Mat frame2;
+	int key;
+	Mat bgr, hsv, mirror, colorDetection, countour;
+	
 	// Główna pętla programu
-	while(true) {
-		
+	while(true) {		
 		
 		if (cap.read(bgr)) {
 			//blur( bgr, bgr, Size(10,10) );
 			cvtColor( bgr, hsv, CV_BGR2HSV); // filtr zmieniający skalę barw
 			flip(hsv, mirror, 1); // Odbicie lustrzane
-			//
-			//blur( mirror, mirror, Size(10,10) );
-			//erode(frame, frame, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
-			//dilate(frame, frame, getStructuringElement(MORPH_ELLIPSE, Size(10, 10)) );
-			inRange (mirror, Scalar (60, 200, 130), Scalar (100, 255, 255), colorDetection);
-			//inRange (frame, Scalar (80, 120, 63), Scalar (120, 240, 83), frame2);
-			//inRange (frame, Scalar (0, 100, 100), Scalar (55, 255, 200), frame2);
+			inRange (mirror, Scalar (60, 200, 130), Scalar (100, 255, 255), colorDetection); // znajdowanie koloru
 			dilate(colorDetection, colorDetection, getStructuringElement(MORPH_ELLIPSE, Size(20, 20)) );
 			erode(colorDetection, colorDetection, getStructuringElement(MORPH_ELLIPSE, Size(20, 20)) );
 			dilate(colorDetection, colorDetection, getStructuringElement(MORPH_ELLIPSE, Size(20, 50)) );
-			dilate(colorDetection, colorDetection, getStructuringElement(MORPH_ELLIPSE, Size(20, 50)) );
+			//dilate(colorDetection, colorDetection, getStructuringElement(MORPH_ELLIPSE, Size(20, 50)) );
 			
 			int jeden, dwa, trzy, cztery;
 				jeden = 15;
@@ -155,70 +129,61 @@ int main( int argc, char** argv ) {
 				trzy = 20;
 				cztery = 3;
 			Canny( colorDetection, colorDetection, jeden, dwa * trzy, cztery ); // Tworzenie konturu
+			
 			vector <vector <Point> > contours;
-			findContours( colorDetection, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);	
-			drawContours( colorDetection, contours, 0, Scalar(255,0,0),5,8);
+			findContours (colorDetection, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);	
+			drawContours (colorDetection, contours, 0, Scalar(255,0,0),1,8);
 			
 			
 			/// Momenty :)
-		vector<Moments> mu(contours.size() );
-		for( int i = 0; i < contours.size(); i++ ) {
-			mu[i] = moments( contours[i], false ); 
-		}
-        
-		
-		if (mu.size() > 0) {
-			Point2f mc; // mass center
-			int maxI = 0;
-			
-			int x, y, przesuniecie;
-						x = mu[maxI].m10/mu[maxI].m00;
-						y = mu[maxI].m01/mu[maxI].m00;
-						przesuniecie = 200;
-					//cout << ((int)  aktualnyCzas / 1000000)<< "   " << clock() <<  endl;
-					if (pozPocz == -1) {
-						pozPocz = x;
-					}					
-					if ((x < 250) && (((double) (clock () - aktualnyCzas) / CLOCKS_PER_SEC) >= 0.5)) {
-						cout << "LEWO" << endl;
-						//imshow (*iter, imread (*iter, CV_LOAD_IMAGE_COLOR));
-						pozPocz = x;
-						aktualnyCzas = clock();
-						showPicture(false);
-					}
-					if ((x > 250) && (((double) (clock () - aktualnyCzas) / CLOCKS_PER_SEC) >= 0.5)) {
-						cout << "PRAWO" << endl;
-						pozPocz = x;
-						aktualnyCzas = clock();
-						
-						showPicture(true);
-					}
-			
-			
-			mc = Point2f( x , y );
+			vector<Moments> mu(contours.size() );
 			for( int i = 0; i < contours.size(); i++ ) {
-				if (contourArea(contours[maxI]) < contourArea(contours[i])) {
-					maxI = i;
-					mc = Point2f( mu[maxI].m10/mu[maxI].m00, mu[maxI].m01/mu[maxI].m00);
-					
-					
-					
-				} 
+				mu[i] = moments( contours[i], false ); 
 			}
-			//circle( colorDetection, mc, sqrt(contourArea(contours[maxI])), Scalar(255,0,0) );
-		}
-		
+        		
+			if (mu.size() > 0) {
+				Point2f mc; // mass center
+				int maxI = 0;
+				
+				int x, y;
+					x = mu[maxI].m10/mu[maxI].m00;
+					y = mu[maxI].m01/mu[maxI].m00;
+				
+				// Kolejne lub poprzednie zdjęcie
+				if ((x < width / 2) && (((double) (clock () - currentTime) / CLOCKS_PER_SEC) >= 0.8)) {
+					currentTime = clock();
+					showPicture(false);
+				}
+				else if ((x > width / 2) && (((double) (clock () - currentTime) / CLOCKS_PER_SEC) >= 0.8)) {
+					currentTime = clock();						
+					showPicture(true);
+				}			
 			
+				mc = Point2f( x , y );
+				
+				for( int i = 0; i < contours.size(); i++ ) {
+					if (contourArea(contours[maxI]) < contourArea(contours[i])) {
+						maxI = i;
+						mc = Point2f( mu[maxI].m10/mu[maxI].m00, mu[maxI].m01/mu[maxI].m00);					
+					} 
+				}
+			}			
 			
-			imshow ("Wspaniała galeria zdjęć", colorDetection);
-			imshow ("Testowe", mirror);
+			/*
+			namedWindow ("Kształt", CV_WINDOW_AUTOSIZE); // Tworzenie nowego okna
+			namedWindow ("BGR",  CV_WINDOW_AUTOSIZE);
+			imshow ("Kształt", colorDetection);
+			imshow ("BGR", mirror);
+			*/
 		}
 		else
 			break;
 		
 		// Czekaj na klawisz, jeżeli jest to 'esc' to kończ pracę
-		if(waitKey(1000.0/fps) == 1048603)
+		//if(waitKey(1000.0/fps) == 1048603)
+		if(waitKey(1000.0/fps) != -1) {
 			break;
+		}
 	}
 	return 0;
 }
